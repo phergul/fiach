58403adb7c04cd198ee31c6fa8e266d707ab7c18
Fergal McLaughlin <fergalserver@outlook.com>
2026-05-10 13:34:23 +0100

    Initial Commit
    Project setup

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"embed"
+	_ "embed"
+	"log"
+
+	"github.com/wailsapp/wails/v3/pkg/application"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	app := application.New(application.Options{
+		Name:        "mod-manager",
+		Description: "General Mod Manager",
+		Services: []application.Service{
+			application.NewService(&GreetService{}),
+		},
+		Assets: application.AssetOptions{
+			Handler: application.AssetFileServerFS(assets),
+		},
+		Mac: application.MacOptions{
+			ApplicationShouldTerminateAfterLastWindowClosed: true,
+		},
+	})
+
+	app.Window.NewWithOptions(application.WebviewWindowOptions{
+		Title: "Manager",
+		Mac: application.MacWindow{
+			InvisibleTitleBarHeight: 50,
+			Backdrop:                application.MacBackdropTranslucent,
+			TitleBar:                application.MacTitleBarHiddenInset,
+		},
+		BackgroundColour: application.NewRGB(27, 38, 54),
+		URL:              "/",
+	})
+
+	err := app.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
