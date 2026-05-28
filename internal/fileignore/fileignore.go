e9d6249e585f2558cc6c915631f7d9f6ca70b757
Fergal McLaughlin <fergalserver@outlook.com>
2026-05-28 16:38:24 +0100

    Mac things

--- internal/fileignore/fileignore.go
+++ internal/fileignore/fileignore.go
@@ -1,10 +1,18 @@
 package fileignore
 
+import "strings"
+
 var ignoredRegistry = map[string]struct{}{
 	".DS_Store": {},
+	"__MACOSX":  {},
 }
 
 func Has(name string) bool {
+	// macos junk
+	if strings.HasPrefix(name, "._") {
+		return true
+	}
+
 	_, ignored := ignoredRegistry[name]
 	return ignored
 }
